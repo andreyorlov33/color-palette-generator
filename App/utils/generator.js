b0c000b6ec3db0a02c89c30cddac4012dbc063ee
Zurg <Zurg@Starlight.local>
2018-01-24 17:38:56 -0800

    finished svg parsing and replasment module

--- App/utils/generator.js
+++ App/utils/generator.js
@@ -1,3 +1,5 @@
+import fs from 'fs'
+
 import RGB_to_HSL from '../conversion_functions/rgb_to_hsl'
 import RGB_to_HSB from '../conversion_functions/rgb_to_hsb'
 import RGB_to_HEX from '../conversion_functions/rgb_to_hex'
@@ -8,10 +10,45 @@ import HSL_to_RGB from '../conversion_functions/hsl_to_rgb'
 
 export default function generate(asset) {
     let { class_2, class_3, class_4 } = asset
-    
+
     let class_2_progression = generator(class_2)
     let class_3_progression = class_3.base_hex != null ? generator(class_3) : null
     let class_4_progression = class_4.base_hex != null ? generator(class_4) : null
+    
+    let string = fs.readFileSync('/Users/Zurg/Desktop/color-palet-tool/SVG/'+asset.asset_name, 'utf8')
+    
+    let div_array = []
+    let dir_name = `/Users/Zurg/Desktop/color-palet-tool/OUTPUT/${asset.asset_name.split('.').shift()}/`
+    if(!fs.existsSync(dir_name)){
+        fs.mkdirSync(dir_name)
+    }
+    
+    for(let i = 0; i < 10; i++){
+        
+        let class_2_hex = class_2_progression[i]
+        let index_2 = string.indexOf('.cls-2{f')+12
+        let hex_substring_2 = string.substring(index_2, index_2+7)
+        let new_svg =  string.replace(hex_substring_2, class_2_progression[i])
+
+        if(class_3.base_hex != null){
+            let class_3_hex = class_3_progression[i]
+            let index_3 = string.indexOf('.cls-3{f')+12
+            let hex_substring_3 = string.substring(index_3, index_3+7)
+            new_svg = new_svg.replace(hex_substring_3, class_3_progression[i])
+        }
+        if(class_4.base_hex != null){
+            let class_4_hex = class_2_progression[i]
+            let index_4 = string.indexOf('.cls-4{f')+12
+            let hex_substring_4 = string.substring(index_4, index_4+7)
+            new_svg = new_svg.replace(hex_substring_4, class_4_progression[i])
+        }
+        let name = asset.asset_name.split('.').shift()
+        fs.writeFileSync(`/Users/Zurg/Desktop/color-palet-tool/OUTPUT/${name}/${name}_${i}.svg`, new_svg, 'utf8')
+    }
+
+    
+
+    
 
 }
 
